88b989f5c0f8e3edcfc881da15bf36c372550c81
ciapjl <CPPJEA001@myuct.ac.za>
2022-04-10 22:55:37 +0200

    Added node and graph Class

--- Graphs/Graph.py
+++ Graphs/Graph.py
@@ -2,5 +2,27 @@
 
 #TODO: define Graph class using 
 
+class Node: 
+    def __init__(self, value, neighbours=None) -> None:
+        self.value = value
+        if neighbours == None:
+            self.neighbours = []
+        else:
+            self.neighbours = neighbours;
 
+    def addNeighbour(self, neighbour):
+        self.neighbours.append(neighbour)
 
+    
+
+class Graph: 
+    def __init__(self, nodes=None) -> None:
+        ##initialize empty 
+        if nodes is None:
+            self.nodes = []
+        else:
+            self.nodes = nodes
+        
+
+    def addNode(self, node):
+        self.nodes.append(Node)
