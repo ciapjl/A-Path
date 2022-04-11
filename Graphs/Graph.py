5de8644e5c524795fb76839b76172e0df00f129f
ciapjl <CPPJEA001@myuct.ac.za>
2022-04-11 10:53:45 +0200

    Added Graph class with various methods

--- Graphs/Graph.py
+++ Graphs/Graph.py
@@ -1,17 +1,5 @@
+from Node import *
 
-
-#TODO: define Graph class using 
-
-class Node: 
-    def __init__(self, value, neighbours=None) -> None:
-        self.value = value
-        if neighbours == None:
-            self.neighbours = []
-        else:
-            self.neighbours = neighbours;
-
-    def addNeighbour(self, neighbour):
-        self.neighbours.append(neighbour)
 
     
 
@@ -22,7 +10,35 @@ class Graph:
             self.nodes = []
         else:
             self.nodes = nodes
+    
+    def addNode(self, node):
+        self.nodes.append(node)
+
+    def findNode(self, value):
+        for node in self.nodes: 
+            if node.value == value:
+                return True
+        return None
+
+    def addEdge(self, value1, value2):
+        node1 = self.findNode(value1)
+        node2 = self.findNode(value2)
+
+        if node1 != None and node2!= None:
+            node1.addNeighbour(node2)
+            node2.addNeighbour(node1)
+        else:
+            raise ValueError(f'No pair of Nodes with values {value1} and {value2} were found')
         
 
-    def addNode(self, node):
-        self.nodes.append(Node)
+
+
+    def areConnected(self, node1, node2):
+        if node1 in node2.neighbours or node2 in node1.neighbours:
+            return True
+        return False
+    
+        
+
+  
+    
